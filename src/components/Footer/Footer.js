333dadba77e9735c29c58d7b280da6da1ec1a1de
Kumiho <pavleta.taseva@gmail.com>
2022-02-16 12:11:21 +0200

    circular items updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,8 +5,11 @@ function Footer() {
     return (
         <footer>
             <small className='footer-small'>
+            <small className='footer-small'>
                 Copyright &copy; 2022 | Pavleta Taseva | All rights reserved<br />
-                <a href='https://www.freepik.com/psd/woman'>Woman psd created by freepik - www.freepik.com</a>
+                <a href='https://www.freepik.com/psd/woman'>Woman psd created by freepik - www.freepik.com</a><br></br>
+                Credits to <a className="links" href="https://www.youtube.com/channel/UCbwXnUipZsLfUckBPsC7Jog" target="_blanc"> Online Tutorials - YouTube Channel</a>        
+            </small>
             </small>
         </footer>
     )
